dc2c3ed75f478dc703f5a217430ff933543f22ce
anith18 <anithraju174@gmail.com>
2025-03-11 18:53:34 +0530

    latest

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,10 @@ app.use(express.json())
 // Available Routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
-
+app.get('/',(req,res)=>{
+  res.send('working...')
+}
+)
 
 app.listen(port, () => {
   console.log(`iNotebook backend listening at http://localhost:${port}`)
